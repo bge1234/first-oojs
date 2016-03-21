7ba96d20b6df03931d3f421ddf0e61fe411ab097
Ben Ebel <bge1234@gmail.com>
2016-03-21 11:29:04 -0600

    added to car constructor

--- car.js
+++ car.js
@@ -1,7 +1,10 @@
-function Car(make) {
+function Car(make, gasLevel, isOn, mileage) {
   this.make = make;
+  this.gasLevel = gasLevel;
+  this.isOn = isOn;
+  this.mileage = mileage;
 }
 
-var prius = new Car("toyota");
+var prius = new Car("toyota", 14, true, 15500);
 
 console.log(prius);
